8b2252929fd0a20e27fa608290a7a4b9fa86d464
ScopeyGameDev <57968756+ScopeyGameDev@users.noreply.github.com>
2021-04-12 12:52:35 +1000

    Navigation Between Pages. Need to allow the backbutton to work according to this.

--- Assets/Flex/Scripts/NavigationManager.cs
+++ Assets/Flex/Scripts/NavigationManager.cs
@@ -3,36 +3,136 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using TMPro;
 
 namespace Flex.Navigation
 {
+    enum PageType { TimeSensitive, Achievements, Guides, GroupFinder, Profile };
+
     public class NavigationManager : MonoBehaviour
     {
         [SerializeField] Button BackButton;
+
+        [Header("TopPanel")]
+        [SerializeField] TMP_Text PageTitle;
+
+        PageType SelectedPage;
+
+        [SerializeField] Sprite OpenBox;
+        [SerializeField] Sprite ClosedBox;
+
+        [Header("TimeSensitive")]
+        [SerializeField] Button TimeSensitiveNavButton;
+        [SerializeField] Image TimeSensitiveNavButtonBorder;
+        [SerializeField] GameObject TimeSensitiveContentPanel;
+
+        [Header("Achievements")]
+        [SerializeField] Button AchievementsNavButton;
+        [SerializeField] Image AchievementsNavButtonBorder;
+        [SerializeField] GameObject AchievementsContentPanel;
+
+        [Header("Guides")]
+        [SerializeField] Button GuidesNavButton;
+        [SerializeField] Image GuidesNavButtonBorder;
+        [SerializeField] GameObject GuidesContentPanel;
+
+        [Header("GroupFinder")]
+        [SerializeField] Button GroupFinderNavButton;
+        [SerializeField] Image GroupFinderNavButtonBorder;
+        [SerializeField] GameObject GroupFinderContentPanel;
+
+        [Header("Profile")]
+        [SerializeField] Button ProfileNavButton;
+        [SerializeField] GameObject ProfileContentPanel;
 
         // Start is called before the first frame update
         void Start()
         {
             BackButton.gameObject.SetActive(false);
-        }
+            TimeSensitiveNavButton.onClick.AddListener(delegate {SwitchPage(PageType.TimeSensitive);} );
+            AchievementsNavButton.onClick.AddListener(delegate {SwitchPage(PageType.Achievements);} );
+            GuidesNavButton.onClick.AddListener(delegate {SwitchPage(PageType.Guides);} );
+            GroupFinderNavButton.onClick.AddListener(delegate {SwitchPage(PageType.GroupFinder);} );
+            ProfileNavButton.onClick.AddListener(delegate {SwitchPage(PageType.Profile);} );
 
-        // Update is called once per frame
-        void Update()
-        {
-            
+            SwitchPage(PageType.TimeSensitive);
         }
 
         internal void SetBackButton(UnityAction Event)
         {
             BackButton.gameObject.SetActive(true);
-            BackButton.onClick.AddListener(delegate {BackButtonPressed(Event);});
+            BackButton.onClick.AddListener(Event);
+            BackButton.onClick.AddListener(delegate {BackButtonPressed();});
         }
 
-        void BackButtonPressed(UnityAction Event)
+        void AddEventToBackButton(UnityAction Event)
         {
-            Event.Invoke();
+            BackButton.onClick.AddListener(Event);
+        }
+
+        void BackButtonPressed()
+        {
+            BackButton.onClick.Invoke();
             BackButton.onClick.RemoveAllListeners();
             BackButton.gameObject.SetActive(false);
+        }
+
+        internal void SwitchPage(PageType NewPageType)
+        {
+            // AddEventToBackButton(delegate {SwitchPage(SelectedPage);});
+
+            switch (SelectedPage)
+            {
+                case PageType.TimeSensitive:
+                    TimeSensitiveNavButtonBorder.sprite = ClosedBox;
+                    TimeSensitiveContentPanel.SetActive(false);
+                    break;
+                case PageType.Achievements:
+                    AchievementsNavButtonBorder.sprite = ClosedBox;
+                    AchievementsContentPanel.SetActive(false);
+                    break;
+                case PageType.Guides:
+                    GuidesNavButtonBorder.sprite = ClosedBox;
+                    GuidesContentPanel.SetActive(false);
+                    break;
+                case PageType.GroupFinder:
+                    GroupFinderNavButtonBorder.sprite = ClosedBox;
+                    GroupFinderContentPanel.SetActive(false);
+                    break;
+                case PageType.Profile:
+                    ProfileContentPanel.SetActive(false);
+                    break; 
+            }
+
+            switch (NewPageType)
+            {
+                case PageType.TimeSensitive:
+                    PageTitle.text = "Time Sensitive";
+                    TimeSensitiveNavButtonBorder.sprite = OpenBox;
+                    TimeSensitiveContentPanel.SetActive(true);
+                    break;
+                case PageType.Achievements:
+                    PageTitle.text = "Achievements";
+                    AchievementsNavButtonBorder.sprite = OpenBox;
+                    AchievementsContentPanel.SetActive(true);
+                    break;
+                case PageType.Guides:
+                    PageTitle.text = "Guides";
+                    GuidesNavButtonBorder.sprite = OpenBox;
+                    GuidesContentPanel.SetActive(true);
+                    break;
+                case PageType.GroupFinder:
+                    PageTitle.text = "GroupFinder";
+                    GroupFinderNavButtonBorder.sprite = OpenBox;
+                    GroupFinderContentPanel.SetActive(true);
+                    break;
+                case PageType.Profile:
+                    PageTitle.text = "Profile";
+                    ProfileContentPanel.SetActive(true);
+                    break; 
+            }
+
+            SelectedPage = NewPageType;
         }
     }
 }
